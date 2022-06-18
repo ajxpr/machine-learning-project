11e6396a66200b9571c7a20bacefa206838a58d8
thezillionaireboy <akshaj.piri@gmail.com>
2022-06-18 11:40:28 +0530

    updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_requirements_list()->List[str]:
 
 # Declaring variables for setup function
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1",
+VERSION="0.0.3",
 AUTHOR="Akshaj Piri"
 DESCRIPTION="This is a ML Project."
 PACKAGES=["housing"]
@@ -20,7 +20,7 @@ def get_requirements_list()->List[str]:
 
 
     with open (REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 
